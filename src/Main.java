00bd0e1d2250ce84bb649e11ef5b198977f46a28
Melvyn Bauvent <melvyn.bauvent@etu.univ-lyon1.fr>
2023-03-31 14:56:24 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
+    private int test;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
+    }
+
+    public int getTest() {
+        return test;
     }
 }
