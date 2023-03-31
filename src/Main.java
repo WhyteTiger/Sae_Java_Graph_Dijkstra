c8e10b3c815cdab4fcbf8924c112411595108c27
Melvyn Bauvent <melvyn.bauvent@etu.univ-lyon1.fr>
2023-03-31 15:14:58 +0200

    suppression des tests

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 
-    private int test;
     public static void main(String[] args) {
         System.out.println("Hello world");
-    }
-
-    public int getTest() {
-        return test;
-    }
-
-    public void setTest(int test) {
-        this.test = test;
     }
 }
