628032e73ab91ce4e78eaae908e397c4c0f8bb84
biggeri emmanuel <emmanuel.biggeri@etu.univ-lyon1.fr>
2023-04-06 15:58:23 +0200

    fonctionne

--- src/Logiciel/Controleur/LectureFichier.java
+++ src/Logiciel/Controleur/LectureFichier.java
@@ -5,20 +5,19 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class LectureFichier {
-    try{
-        FileReader reader = new FileReader(""); //mettre le chemin vers le fichier
-        int data = reader.read();
-        while(data != -1){
-            System.out.print((char)data);
-            data = reader.read();
+    public void lectureFichier() {
+        try {
+            FileReader reader = new FileReader(""); //mettre le chemin vers le fichier
+            int data = reader.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = reader.read();
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("not found");
+        } catch (IOException e) {
+            System.out.println("err");
         }
-        reader.close();
     }
-        catch(FileNotFoundException e){
-        System.out.println("not found");
-    }
-        catch (IOException e){
-        System.out.println("err");
-    }
-
 }
