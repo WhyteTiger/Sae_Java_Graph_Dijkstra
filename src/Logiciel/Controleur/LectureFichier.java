2bf5c1efa7adcb723c493b812eb6f5e8742e7798
biggeri emmanuel <emmanuel.biggeri@etu.univ-lyon1.fr>
2023-04-06 15:53:42 +0200

    lire un fichier

---
+++ src/Logiciel/Controleur/LectureFichier.java
@@ -0,0 +1,24 @@
+package Logiciel.Controleur;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LectureFichier {
+    try{
+        FileReader reader = new FileReader(""); //mettre le chemin vers le fichier
+        int data = reader.read();
+        while(data != -1){
+            System.out.print((char)data);
+            data = reader.read();
+        }
+        reader.close();
+    }
+        catch(FileNotFoundException e){
+        System.out.println("not found");
+    }
+        catch (IOException e){
+        System.out.println("err");
+    }
+
+}
