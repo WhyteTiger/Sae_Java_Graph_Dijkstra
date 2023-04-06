d05e90a1c3cb5ce595df4a2f7f4790dd3273038e
Melvyn Bauvent <melvyn.bauvent@etu.univ-lyon1.fr>
2023-04-06 18:28:00 +0200

    Suppression des tests

--- src/Logiciel/Vue/Fenetre.java
+++ src/Logiciel/Vue/Fenetre.java
@@ -1,9 +1,4 @@
 package Logiciel.Vue;
 
 public class Fenetre {
-    public void affichage(){
-        System.out.println("hello");
-        System.out.println("bdhbef");
-        System.out.println("uvduyez");
-    }
 }
